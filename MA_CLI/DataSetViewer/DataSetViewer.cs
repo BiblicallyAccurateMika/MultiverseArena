3f003e37a134a81c226e889beeae91e19911fe50
IgamiShin <michael.brewka@gmx.net>
2025-04-24 11:54:09 +0200

    added levels to unitview

--- MA_CLI/DataSetViewer/DataSetViewer.cs
+++ MA_CLI/DataSetViewer/DataSetViewer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using Ma_CLI.Util;
 using MA_Core.Abstract;
 using MA_Core.Data;
 using MA_Core.Logic.ProcessManagers.DataSetViewer;
@@ -191,7 +192,7 @@ public static class DataSetViewer
 
     private class ActionDetailView : View
     {
-        protected override string ViewName => _action.Name;
+        protected override string ViewName => _action.ToCliShortString();
         private readonly Action _action;
         
         public ActionDetailView(DataSet data, View parent, string id) : base(data, parent)
@@ -227,6 +228,10 @@ public static class DataSetViewer
     private class UnitDetailView : View
     {
         protected override string ViewName => _unit.Codename;
+        
+        protected override Interaction[] Interactions => 
+            [new("Action ID", "Action Details", id => new ActionDetailView(Data, this, id!), id => Data.Actions.Any(x => x.ID == id))];
+        
         private readonly Unit _unit;
         
         public UnitDetailView(DataSet data, View parent, string codename) : base(data, parent)
@@ -238,8 +243,78 @@ public static class DataSetViewer
         {
             builder.AppendLine($"Codename: {_unit.Codename}");
             builder.AppendLine($"Icon Path: {_unit.IconPath}");
+            builder.AppendLine();
+            builder.Append(renderLevelComparison());
+        }
+
+        private string renderLevelComparison()
+        {
+            var levels = new[] { _unit.Level1, _unit.Level2, _unit.Level3, _unit.Level4 };
+            List<int> columnWidths = ["SpritePath".Length];
+            columnWidths.AddRange(levels.Select(x => new[]
+            {
+                7, // Header width -> "Level x"
+                x.Name.Length,
+                x.SpritePath.Length,
+                x.HP.ToString().Length,
+                x.Strength.ToString().Length,
+                x.Toughness.ToString().Length,
+                x.Precision.ToString().Length,
+                x.Agility.ToString().Length,
+                x.Power.ToString().Length,
+                x.Defense.ToString().Length,
+                x.Aura.ToString().Length,
+                x.Willpower.ToString().Length,
+                x.Actions.Max(y => $"{y.ID} - {y.Name}".Length)
+            }.Max()));
+
+            var divider = "+" + String.Join("+", columnWidths.Select(width => new string('-', width + 2))) + "+";
             
-            //todo: Show level detail
+            var sb = new StringBuilder();
+            
+            appendDivider();
+            appendLine("", "Level 1", "Level 2", "Level 3", "Level 4");
+            appendDivider();
+            appendLine("Name", _unit.Level1.Name, _unit.Level2.Name, _unit.Level3.Name, _unit.Level4.Name);
+            appendLine("SpritePath", _unit.Level1.SpritePath, _unit.Level2.SpritePath, _unit.Level3.SpritePath, _unit.Level4.SpritePath);
+            appendDivider();
+            appendLine("HP", _unit.Level1.HP.ToString(), _unit.Level2.HP.ToString(), _unit.Level3.HP.ToString(), _unit.Level4.HP.ToString());
+            appendLine("Strength", _unit.Level1.Strength.ToString(), _unit.Level2.Strength.ToString(), _unit.Level3.Strength.ToString(), _unit.Level4.Strength.ToString());
+            appendLine("Toughness", _unit.Level1.Toughness.ToString(), _unit.Level2.Toughness.ToString(), _unit.Level3.Toughness.ToString(), _unit.Level4.Toughness.ToString());
+            appendLine("Precision", _unit.Level1.Precision.ToString(), _unit.Level2.Precision.ToString(), _unit.Level3.Precision.ToString(), _unit.Level4.Precision.ToString());
+            appendLine("Agility", _unit.Level1.Agility.ToString(), _unit.Level2.Agility.ToString(), _unit.Level3.Agility.ToString(), _unit.Level4.Agility.ToString());
+            appendLine("Power", _unit.Level1.Power.ToString(), _unit.Level2.Power.ToString(), _unit.Level3.Power.ToString(), _unit.Level4.Power.ToString());
+            appendLine("Defense", _unit.Level1.Defense.ToString(), _unit.Level2.Defense.ToString(), _unit.Level3.Defense.ToString(), _unit.Level4.Defense.ToString());
+            appendLine("Aura", _unit.Level1.Aura.ToString(), _unit.Level2.Aura.ToString(), _unit.Level3.Aura.ToString(), _unit.Level4.Aura.ToString());
+            appendLine("Willpower", _unit.Level1.Willpower.ToString(), _unit.Level2.Willpower.ToString(), _unit.Level3.Willpower.ToString(), _unit.Level4.Willpower.ToString());
+            appendDivider();
+            var first = true;
+            for (var i = 0; first || i < levels.Max(x => x.Actions.Length); i++)
+            {
+                var header = first ? "Actions" : "";
+                if (first) first = false;
+                
+                var lvl1 = _unit.Level1.Actions.Length > i ? _unit.Level1.Actions[i].ToCliShortString() : "";
+                var lvl2 = _unit.Level2.Actions.Length > i ? _unit.Level2.Actions[i].ToCliShortString() : "";
+                var lvl3 = _unit.Level3.Actions.Length > i ? _unit.Level3.Actions[i].ToCliShortString() : "";
+                var lvl4 = _unit.Level4.Actions.Length > i ? _unit.Level4.Actions[i].ToCliShortString() : "";
+                
+                appendLine(header, lvl1, lvl2, lvl3, lvl4);
+            }
+            appendDivider();
+            
+            return sb.ToString();
+            
+            void appendDivider() => sb.AppendLine(divider);
+            void appendLine(string header, string lvl1, string lvl2, string lvl3, string lvl4)
+            {
+                var paddedHeader = header.PadRight(columnWidths[0]);
+                var paddedLvl1 = lvl1.PadRight(columnWidths[1]);
+                var paddedLvl2 = lvl2.PadRight(columnWidths[2]);
+                var paddedLvl3 = lvl3.PadRight(columnWidths[3]);
+                var paddedLvl4 = lvl4.PadRight(columnWidths[4]);
+                sb.AppendLine($"| {paddedHeader} | {paddedLvl1} | {paddedLvl2} | {paddedLvl3} | {paddedLvl4} |");
+            }
         }
     }
 }
---
+++ MA_CLI/Util/Extensions.cs
@@ -0,0 +1,8 @@
+﻿using Action = MA_Core.Data.Action;
+
+namespace Ma_CLI.Util;
+
+public static class Extensions
+{
+    public static string ToCliShortString(this Action action) => $"{action.ID} - {action.Name}";
+}
