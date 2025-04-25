74d981d3c37b7d1074fee012f299d38d77ba4a0d
IgamiShin <michael.brewka@gmx.net>
2025-04-25 18:09:00 +0200

    added settings to cli

---
+++ MA_CLI/Settings.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+
+namespace Ma_CLI;
+
+public class Settings
+{
+    #region Const
+
+    private const string SettingsFilePath = "settings.json";
+
+    #endregion
+
+    #region Singleton
+
+    private static Settings? _instance;
+    public static Settings Instance => _instance ??= JsonSerializer.Deserialize<Settings>(File.ReadAllText(SettingsFilePath))!;
+
+    private Settings() { }
+
+    #endregion
+
+    #region Settings
+
+    public string DataSetFolderPath { get; init; } = null!;
+
+    #endregion
+
+    #region Methods
+
+    public void Save()
+    {
+        File.WriteAllText(SettingsFilePath, JsonSerializer.Serialize(this));
+    }
+
+    #endregion
+}
