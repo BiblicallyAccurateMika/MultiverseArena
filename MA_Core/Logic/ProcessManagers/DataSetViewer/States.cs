b9649450bcc0c0f55ecf43aea9f4b1653c81563b
IgamiShin <michael.brewka@gmx.net>
2025-04-26 21:48:48 +0200

    simplified states and removed old DataSetViewer.cs

--- MA_Core/Logic/ProcessManagers/DataSetViewer/States.cs
+++ MA_Core/Logic/ProcessManagers/DataSetViewer/States.cs
@@ -2,23 +2,12 @@
 
 namespace MA_Core.Logic.ProcessManagers.DataSetViewer;
 
-public abstract class DataSetViewerState : ICloneable
-{
-    public abstract object Clone();
-}
+public abstract class DataSetViewerState;
 
-public class EmptyState : DataSetViewerState
-{
-    public override object Clone() => new EmptyState();
-}
+public class EmptyState : DataSetViewerState;
 
 public class LoadedState(DataSet dataSet, bool unload = false) : DataSetViewerState
 {
     public DataSet DataSet { get; } = dataSet;
     public readonly bool Unload = unload;
-    
-    public override object Clone()
-    {
-        return new LoadedState(DataSet, Unload);
-    }
 }
